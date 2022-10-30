390680e1beef41c3f396a59d59d5c2102b0d61d1
Kubra Hanedar <the.hanedar@gmail.com>
2022-10-30 18:51:29 +0300

    UserDefaults eklendi

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -1,5 +1,5 @@
-//
-//  ViewController.swift
+
+//  ViewControlle r.swift
 //  BirthdayNoteTaker
 //
 //  Created by Kübra Hanedar on 24.10.2022.
@@ -17,13 +17,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        // casting - as? "yapabilirsen" vs. as! "kesinlikle öyle kaydet"
+        
+        if let newName = storedName as? String {
+            nameLabel.text = newName
+        }
+        if let newBirthday = storedBirthday as? String {
+            birthdayLabel.text = newBirthday
+        }
+        
     }
 
     @IBAction func saveButton(_ sender: Any) {
+        //userDefaults an interface to the user's default database, where you store key-value pairs persistently across launches of your app. "veri tabanına ulaşabilir ve değer kaydedebilir."
+        UserDefaults.standard.set(nameText.text!, forKey: "name")
+        UserDefaults.standard.set(birthdayText.text!, forKey: "birthday")
+        
+        
         nameLabel.text = nameText.text
         birthdayLabel.text = birthdayText.text
+        // stringlerin içinde değişken kullanmak için "Name:\(birthdayTextField.text!)" \paranteze al.
     
     }
     
 }
+
 
